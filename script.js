95f1832f8c2a931866b4eb810e9de20ad95b7931
mati <127502387+nmmateusz@users.noreply.github.com>
2024-11-20 11:26:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-let scores = { score1: 0, score2: 0 };
-let winningScore = 10;
+// wyniki graczy
+let scores = { score1: 0, score2: 0 }; 
+// liczba punktów do wygranej
+let winningScore = 10; 
 
+// funkcja zwiększająca wynik
 function increment(scoreId) {
-    if (scores[scoreId] < winningScore) {
-        scores[scoreId]++;
-        updateScore(scoreId);
+    // sprawdź, czy wynik jest mniejszy od liczby punktów do wygranej
+    if (scores[scoreId] < winningScore) { 
+        // zwiększ wynik o 1
+        scores[scoreId]++; 
+        // aktualizuje wynik na stronie
+        updateScore(scoreId); 
     }
-    checkWinner();
+    checkWinner(); // sprawdź, czy ktoś wygrał
 }
 
 function decrement(scoreId) {
@@ -17,57 +23,75 @@ function decrement(scoreId) {
     checkWinner();
 }
 
+//aktualizacja wyswietlanego wyniku 
 function updateScore(scoreId) {
     document.getElementById(scoreId).textContent = scores[scoreId];
     updateLeader();
 }
 
+//update leadera 
 function updateLeader() {
     const leader = scores.score1 > scores.score2 ? 'Marek' :
                    scores.score2 > scores.score1 ? 'Kobos' : 'nikt';
     document.getElementById('leader').textContent = leader;
 }
 
+//funckja sprawdzajaca czy ktoś wygral
 function checkWinner() {
-    if (scores.score1 >= winningScore || scores.score2 >= winningScore) {
-        // po wygranej zmienia na obraz wygranego
-        let winnerId = scores.score1 >= winningScore ? 'player1' : 'player2';
-        let winnerImage = winnerId === 'player1' ? 'marekpwin.jpg' : 'koboswin.png';
-        let audio = new Audio('piekneoczy.mp3');
-        audio.play();
-        document.getElementById(winnerId).style.backgroundImage = `url('${winnerImage}')`;
-        let winner = winnerId === 'player1' ? 'Gracz 1' : 'Gracz 2';
-        alert(winner + " jest kozakiem!!1!!11!");
-        disableButtons();
+     // jeśli któryś z graczy osiągnął wymagany wynik
+     if (scores.score1 >= winningScore || scores.score2 >= winningScore) { 
+        // okresla wygranego
+        let winnerId = scores.score1 >= winningScore ? 'player1' : 'player2'; 
+        // wybór image wygranego
+        let winnerImage = winnerId === 'player1' ? 'marekpwin.jpg' : 'koboswin.png'; 
+        // odtwarzanie muzy
+        let audio = new Audio('piekneoczy.mp3'); 
+        audio.play(); 
+        // ustaw tło z obrazkiem zwycięzcy
+        document.getElementById(winnerId).style.backgroundImage = `url('${winnerImage}')`; 
+        // wybiera tekst z gratulacjami dla wygranego
+        let winner = winnerId === 'player1' ? 'gracz 1' : 'gracz 2'; 
+        // pokaż komunikat 
+        alert(winner + " jest kozakiem!!1!!11!"); 
+        disableButtons(); // wyłącz przyciski
 
         //czas resetu po kliknieciu ok
         setTimeout(resetGame, 1000000);
     }
 }
-
+//funkcja wylaczajaca wszystkie przyciski
 function disableButtons() {
     document.querySelectorAll("button").forEach(button => {
         button.disabled = true;
     });
 }
 
+//funkcja wlaczajaca wszystkie przyciski
 function enableButtons() {
     document.querySelectorAll("button").forEach(button => {
         button.disabled = false;
     });
 }
 
+// funkcja resetująca gre
 function resetGame() {
-    scores = { score1: 0, score2: 0 };
-    document.getElementById('score1').textContent = 0;
-    document.getElementById('score2').textContent = 0;
-    document.getElementById('player1').classList.remove('winner');
-    document.getElementById('player2').classList.remove('winner');
+    // resetuj wyniki
+    scores = { score1: 0, score2: 0 }; 
+    // ustaw wynik gracza 1 na 0
+    document.getElementById('score1').textContent = 0; 
+    // ustaw wynik gracza 2 na 0
+    document.getElementById('score2').textContent = 0; 
+    // usuń klasę 'winner' dla gracza 1
+    document.getElementById('player1').classList.remove('winner'); 
+    // usuń klasę 'winner' dla gracza 2
+    document.getElementById('player2').classList.remove('winner'); 
     enableButtons();
-    updateLeader();
+    updateLeader(); 
 }
 
+// funkcja zmieniająca liczbę punktów do wygranej
 document.getElementById('winningScore').addEventListener('input', function() {
+    // ustaw wartość wygranej
     winningScore = parseInt(this.value) || 10;
-    resetGame();
+    resetGame();// resetuj grę po zmianie wygranej
 });
